2769d3fce4ea506cbc36a102911872068ef066b8
ucwong <ucwong@126.com>
2019-12-11 15:34:34 +0800

    param change

--- consensus/cuckoo/consensus.go
+++ consensus/cuckoo/consensus.go
@@ -404,7 +404,7 @@ func calcDifficultyByzantium(time uint64, parent *types.Header) *big.Int {
 
 	// (2 if len(parent_uncles) else 1) - (block_timestamp - parent_timestamp) // 9
 	x.Sub(bigTime, bigParentTime)
-	x.Div(x, big9)
+	x.Div(x, big1)
 	if parent.UncleHash == types.EmptyUncleHash {
 		x.Sub(big1, x)
 	} else {
